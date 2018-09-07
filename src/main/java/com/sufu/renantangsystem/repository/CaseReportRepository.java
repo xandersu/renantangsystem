fc95b415e15f740cde4cc34f43cd67b5f6072c3b
xandersu <840595532@qq.com>
2018-09-07 09:01:50 +0800

    add MyMapper

--- src/main/java/com/sufu/renantangsystem/repository/CaseReportRepository.java
+++ src/main/java/com/sufu/renantangsystem/repository/CaseReportRepository.java
@@ -1,14 +1,14 @@
 package com.sufu.renantangsystem.repository;
 
 import com.sufu.renantangsystem.entity.CaseReportEntity;
-import tk.mybatis.mapper.common.Mapper;
+import com.sufu.renantangsystem.util.MyMapper;
 
 import java.util.List;
 
 /**
  * Created by lenovo on 2018/6/18.
  */
-public interface CaseReportRepository extends Mapper<CaseReportEntity> {
+public interface CaseReportRepository extends MyMapper<CaseReportEntity> {
 
     List<CaseReportEntity> findAllByPatientIdOrderByClinicTimeDesc(String patientId);
 
--- src/main/java/com/sufu/renantangsystem/repository/PatientRepository.java
+++ src/main/java/com/sufu/renantangsystem/repository/PatientRepository.java
@@ -1,12 +1,12 @@
 package com.sufu.renantangsystem.repository;
 
 import com.sufu.renantangsystem.entity.PatientEntity;
-import tk.mybatis.mapper.common.Mapper;
+import com.sufu.renantangsystem.util.MyMapper;
 
 /**
  * Created by lenovo on 2018/6/18.
  */
-public interface PatientRepository extends Mapper<PatientEntity> {
+public interface PatientRepository extends MyMapper<PatientEntity> {
 
 
 }
